e35db1c2c0ecda47a4868bf197da3b929961007d
Alejandro <aejandro.cortesmarin89@gmail.com>
2025-07-26 19:14:29 +0200

    Interface GraphInterface

--- core/graph_interface.h
+++ core/graph_interface.h
@@ -1,4 +1,15 @@
 #ifndef GRAPH_INTERFACE_H
 #define GRAPH_INTERFACE_H
 
+#include <vector>
+
+class GraphInterface {
+public:
+    virtual ~GraphInterface() = default;
+    virtual int nodeCount() const = 0;
+    virtual double distance(int i, int j) const = 0;
+    virtual std::vector<int> getNodeIds() const = 0;
+};
+
+
 #endif // GRAPH_INTERFACE_H
